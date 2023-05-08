0636e3d2c3e40dcc78b2cd0d637c11d48b7d5377
Anna Sutheim <asutheim@gmail.com>
2023-05-08 14:04:55 -0500

    generateNew function adds yellow and delete buttons to DOM when generate button is clicked

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ function onReady() {
 function generateNew() {
   event.preventDefault();
   console.log("Generate button clicked");
-  
+  $(`div.appendStuffHere`).append(`
+    <div class="new">
+      <p>
+        <!-- something displaying the clicks count-->
+        <button class="yellow-button">Yellow</button>
+        <button class="delete-button">Delete</button>
+      </p>
+    </div>
+  `);
+  countClicks();
 }
+
+let clicks = 0;
+function countClicks() {
+  console.log("This works?");
+  clicks++;
+  console.log(clicks);
+}
+
