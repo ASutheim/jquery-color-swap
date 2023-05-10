8baf5ae61e9e3c86b195a82514cd4bbcf8b06b4f
Anna Sutheim <asutheim@gmail.com>
2023-05-10 18:10:22 -0500

    Fixed mistake that prevented click count from appending

--- script.js
+++ script.js
@@ -6,24 +6,21 @@ function onReady() {
   $(`#appendStuffHere`).on(`click`, `#yellow-button`, yellowButton);
   $(`#appendStuffHere`).on(`click`, `#delete-button`, deleteButton);
 }
+let clicks = 0;
 
 function generateNew(event) {
   event.preventDefault();
+  clicks++;
   console.log("Generate button clicked");
   $(`#appendStuffHere`).append(`
     <div id="generated">
       <p>
+        <text>${clicks}</text>
         <button id="yellow-button">Yellow</button>
         <button id="delete-button">Delete</button>
       </p>
     </div>
   `);
-}
-
-let clicks = 0;
-function countClicks() {
-  clicks++;
-  console.log(clicks);
 }
 
 function yellowButton(event) {
@@ -35,18 +32,3 @@ function deleteButton(event) {
   console.log("Delete clicked");
   $(this).parent().parent().remove();
 }
-
-/*
-
-Stuff I tried to get the countClicks function to display inside the <p> tag:
-
-
-          <embed type=`text/js`, src=`countClicks(clicks).js`>
-
-        $(this).append(countClicks)
-
-        $("appendStuffHere".child).append(countClicks())
-
-        .... and a lot of other stuff I didn't save, but I worked on it for SO LONG . 
-
-*/
